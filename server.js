37761f6a526635b3b8f3f4b4d5a5cbea5ac92af9
SIDNEY SILVA <67187745+sxdny@users.noreply.github.com>
2024-01-31 19:44:34 +0100

    pues eso
    acabar la actividad

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const port = 3000;
+
+// Obtenemos el data del otro archivo
+const getAccessToken = require('./lib/spotify');
+
+let accessToken = await getAccessToken();
 
 app.use(express.static('public')); // carpeta publica
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
-  // console.log(process.env.SPOTIFY_CLIENT_ID);
-  // console.log(process.env.SPOTIFY_CLIENT_SECRET);
+  console.log(accessToken);
 });
